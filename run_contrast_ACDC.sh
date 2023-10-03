819d355a534ba12b801fb71e630dc01fa97ea36d
Andrei Goncharov <goncharov.andrei.de@gmail.com>
2023-10-04 00:58:53 +0200

    train bash scripts added

---
+++ run_contrast_ACDC.sh
@@ -0,0 +1,2 @@
+python train_contrast.py --device cuda:0 --batch_size 32 --epochs 1 --data_dir ACDC/unlabeled --lr 0.1 --do_contrast --dataset acdc --patch_size 272 272 \
+--experiment_name contrast_acdc_pcl_temp01_thresh035_ --slice_threshold 0.35 --temp 0.1 --initial_filter_size 48 --classes 512 --contrastive_method pcl
